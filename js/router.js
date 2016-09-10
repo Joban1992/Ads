7a737bfa10bde1645a19aa887f8fb4165182089f
Joban1992 <singh.jobanpreet@tcs.com>
2016-09-10 13:42:22 +0530

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,7 +1,12 @@
-angular.module("app.router",[]).config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider){
-    
+angular.module("app.router", []).config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    // Turn off caching for demo simplicity's sake
     $ionicConfigProvider.views.maxCache(0);
-   
+
+    /*
+     // Turn off back button text
+     $ionicConfigProvider.backButton.previousTitleText(false);
+     */
     $stateProvider.state('app', {
         url: '/app',
         abstract: true,
@@ -9,91 +14,103 @@ angular.module("app.router",[]).config(function($stateProvider, $urlRouterProvid
         controller: 'AppCtrl'
     })
 
-    .state('app.activity', {
-        url: '/activity',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/activity.html',
-                controller: 'ActivityCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-activity" class="button button-fab button-fab-top-right expanded button-energized-900 spin" ng-click="serachFabBtnClicked()"><i class="icon" ng-class="searchScreen.searchBtnIcon"></i></button>',
-                controller: function ($timeout) {
-                    $timeout(function () {
-                        document.getElementById('fab-activity').classList.toggle('on');
-                    }, 200);
+            .state('app.activity', {
+                url: '/activity',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/activity.html',
+                        controller: 'ActivityCtrl'
+                    },
+                    'fabContent': {
+                        template: '<button id="fab-activity" class="button button-fab button-fab-top-right expanded button-energized-900 spin" ng-click="serachFabBtnClicked()"><i class="icon" ng-class="searchScreen.searchBtnIcon"></i></button>',
+                        controller: function($timeout) {
+                            $timeout(function() {
+                                document.getElementById('fab-activity').classList.toggle('on');
+                            }, 200);
+                        }
+                    }
                 }
-            }
-        }
-    })
+            })
 
-    .state('app.notifications', {
-        url: '/notifications',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/notifications.html',
-                controller: 'NotificationsCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-friends" class="button button-fab button-fab-top-left expanded button-energized-900 spin"><i class="icon ion-chatbubbles"></i></button>',
-                controller: function ($timeout) {
-                    $timeout(function () {
-                        document.getElementById('fab-friends').classList.toggle('on');
-                    }, 900);
+            .state('app.notifications', {
+                url: '/notifications',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/notifications.html',
+                        controller: 'NotificationsCtrl'
+                    },
+                    'fabContent': {
+                        template: '<button id="fab-friends" class="button button-fab button-fab-top-left expanded button-energized-900 spin"><i class="icon ion-chatbubbles"></i></button>',
+                        controller: function($timeout) {
+                            $timeout(function() {
+                                document.getElementById('fab-friends').classList.toggle('on');
+                            }, 900);
+                        }
+                    }
                 }
-            }
-        }
-    })
+            })
 
-    .state('app.myads', {
-        url: '/myads',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/myads.html',
-                controller: 'MyAdsCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-gallery" class="button button-fab button-fab-top-right expanded button-energized-900 drop"><i class="icon ion-heart"></i></button>',
-                controller: function ($timeout) {
-                    $timeout(function () {
-                        document.getElementById('fab-gallery').classList.toggle('on');
-                    }, 600);
+            .state('app.myads', {
+                url: '/myads',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/myads.html',
+                        controller: 'MyAdsCtrl'
+                    },
+                    'fabContent': {
+                        template: '<button id="fab-gallery" class="button button-fab button-fab-top-right expanded button-energized-900 drop"><i class="icon ion-heart"></i></button>',
+                        controller: function($timeout) {
+                            $timeout(function() {
+                                document.getElementById('fab-gallery').classList.toggle('on');
+                            }, 600);
+                        }
+                    }
                 }
-            }
-        }
-    })
+            })
 
-    .state('app.login', {
-        url: '/login',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/login.html',
-                controller: 'LoginCtrl'
-            },
-            'fabContent': {
-                template: ''
-            }
-        }
-    })
-
-    .state('app.profile', {
-        url: '/profile',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/profile.html',
-                controller: 'ProfileCtrl'
-            },
-            'fabContent': {
-                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
-                controller: function ($timeout) {
-                    /*$timeout(function () {
-                        document.getElementById('fab-profile').classList.toggle('on');
-                    }, 800);*/
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/login.html',
+                        controller: 'LoginCtrl'
+                    },
+                    'fabContent': {
+                        template: ''
+                    }
                 }
-            }
-        }
-    })
-    ;
+            })
 
-    $urlRouterProvider.otherwise('/app/login');
+            .state('app.profile', {
+                url: '/profile',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/profile.html',
+                        controller: 'ProfileCtrl'
+                    },
+                    'fabContent': {
+                        template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                        controller: function($timeout) {
+                            /*$timeout(function () {
+                             document.getElementById('fab-profile').classList.toggle('on');
+                             }, 800);*/
+                        }
+                    }
+                }
+            })
+
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html',
+                        controller: 'SearchCtrl'
+                    }
+                }
+
+            })
+            ;
+
+    // if none of the above states are matched, use this as the fallback
+//    $urlRouterProvider.otherwise('/app/login');
 });
