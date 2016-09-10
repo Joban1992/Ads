4fe54e5c36527361d9f27b61b1aa8492361ebc97
Joban1992 <singh.jobanpreet@tcs.com>
2016-09-10 14:51:57 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -230,17 +230,17 @@ angular.module('app.controllers', [])
             $scope.$parent.setHeaderFab(false);
 
         //     // Set Motion
-        //     $timeout(function() {
-        //         ionicMaterialMotion.slideUp({
-        //             selector: '.slide-up'
-        //         });
-        //     }, 300);
+            $timeout(function() {
+                ionicMaterialMotion.slideUp({
+                    selector: '.slide-up'
+                });
+            }, 300);
 
-        //     $timeout(function() {
-        //         ionicMaterialMotion.fadeSlideInRight({
-        //             startVelocity: 3000
-        //         });
-        //     }, 700);
+            $timeout(function() {
+                ionicMaterialMotion.fadeSlideInRight({
+                    startVelocity: 3000
+                });
+            }, 700);
 
             // Set Ink
             ionicMaterialInk.displayEffect();
