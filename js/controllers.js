37546ea07c0e53f0a3616243168e981d7f5f4ab1
Joban1992 <singh.jobanpreet@tcs.com>
2016-09-11 16:54:08 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -221,46 +221,6 @@ angular.module('app.controllers', [])
 
         })
 
-        .controller('SearchCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
-            $scope.$parent.searchScreen.searchBoxVisible = false;
-            // Set Header
-            $scope.$parent.showHeader();
-            $scope.$parent.clearFabs();
-            $scope.isExpanded = false;
-            $scope.$parent.setExpanded(false);
-            $scope.$parent.setHeaderFab(false);
-
-            // Set Motion
-//            $timeout(function() {
-//                ionicMaterialMotion.slideUp({
-//                    selector: '.slide-up'
-//                });
-//            }, 300);
-
-            $timeout(function() {
-                ionicMaterialMotion.fadeSlideInRight({
-                    startVelocity: 3000
-                });
-            }, 700);
-
-            // Set Ink
-            ionicMaterialInk.displayEffect();
-
-            $scope.putAtTop = "";
-            $scope.searchResultFlag = false;
-            $scope.searchBtnClicked = function() {
-                console.log('ss');
-                $scope.putAtTop = "put-at-top";
-                console.log($scope.putAtTop);
-                $timeout(function() {
-                    document.getElementById('fab-activity').classList.toggle('on');
-                }, 200);
-                
-                $timeout(function() {
-                    $scope.searchResultFlag = true;
-                }, 400);
-                
-            }
-        })
+        
 
         ;
